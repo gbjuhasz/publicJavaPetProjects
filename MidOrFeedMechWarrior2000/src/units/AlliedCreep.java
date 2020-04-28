a4fa967399041eae4da0c660eddf8d6d92eb5173
gbjuhasz <juhasz.gabor.bence@gmail.com>
2020-04-28 23:25:31 +0200

    image files added for allied creeps

---
+++ MidOrFeedMechWarrior2000/src/units/AlliedCreep.java
@@ -0,0 +1,8 @@
+package units;
+
+public class AlliedCreep extends Creep {
+  //fields for positioning on board, image picking and drawing effects
+  private String facingDirection = "Up";
+  private String unitTypeForImage = "AlliedCreep";
+
+}
